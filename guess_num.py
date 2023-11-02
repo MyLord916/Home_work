32dc36f7babde1a58fea8129aae8bca7af193acb
MyLord916 <frimancap@yandex.ru>
2023-11-02 21:17:24 +0500

    add some fix

--- guess_num.py
+++ guess_num.py
@@ -5,8 +5,8 @@ def state_poling():
     global state, rd_num, attempt, set_num
     if state == 'start':
         rd_num = randint(1, set_num)
-        print(rd_num)
-        print(f'Загаданно число от 1 до {set_num}, попробуй отгадать\n')
+        #        print(rd_num)  #Подсказка для отладки
+        print(f'\nЗагаданно число от 1 до {set_num}, попробуй отгадать\n')
         state = 'game'
         return
     elif state == 'game':
@@ -28,7 +28,7 @@ def state_poling():
                 print('\nХотите сыграть заново? (Y / N)\n')
                 return
         elif answer.upper() == 'S':
-            print('Можете настроить парраметры игры, укажите предел загаданного числа:', end=' ')
+            print('\nМожете настроить парраметры игры, укажите предел загаданного числа:', end=' ')
             state = 'settings'
             return
         else:
@@ -44,7 +44,7 @@ def state_poling():
             state = 'exit'
             return
         elif answer.upper() == 'S':
-            print('Можете настроить парраметры игры, укажите предел загаданного числа:', end=' ')
+            print('\nМожете настроить парраметры игры, укажите предел загаданного числа:', end=' ')
             state = 'settings'
             return
         else:
@@ -58,7 +58,7 @@ def state_poling():
             attempt = 0
             return
         else:
-            print('Нужно указать целое число больше 1')
+            print('\nНужно указать целое число больше 1\n')
             return
 
 
@@ -68,7 +68,8 @@ rd_num = None
 attempt = 0
 
 print('Добро пожаловать в числовую угадайку!\n')
-print(state)
+
 while state != 'exit':
     state_poling()
+
 print('\nСпасибо, что играли в числовую угадайку. Еще увидимся...')
